4e6b698748d9cdb7706fce738e58e819bacfe7a2
Ashal aka JOKER <ahastudio@gmail.com>
2024-10-21 05:33:26 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Wild Backend Shipping application
+
+## 테스트 실행
+
+```bash
+./gradlew test
+```
+
+## 애플리케이션(서버) 실행
+
+```bash
+./gradlew bootRun
+```
---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.3.4"
+    id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "com.example.demo"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
